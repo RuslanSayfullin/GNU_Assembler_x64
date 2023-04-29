1b1edf7fbffd242e35606fadda51c8a69cf97592
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-04-29 22:36:26 +0500

    GNU Assembler x64. Разделение команд и комментарии

--- test.s
+++ test.s
@@ -1,17 +1,21 @@
-.file "test.s"
-.data
-str: .ascii "test\n"
+    .file "test.s"
+    .data
+str: 
+    .ascii "test\n"
 
-.text
-.global _start
+    .text
+    .global _start
 _start:
-movq $1, %rax
-movq $1, %rdi
-movq $str, %rsi
-movq $5, %rdx
-syscall
 
-movq $60, %rax
-movq $0, %rdi
-syscall
+    // print "test\n"
+    movq $1, %rax   ;// sysy_write
+    movq $1, %rdi
+    movq $str, %rsi
+    movq $5, %rdx
+    syscall
+
+    // exit
+    movq $60, %rax
+    movq $0, %rdi
+    syscall
 
